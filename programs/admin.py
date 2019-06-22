3745c96c28a0d478a274a91cf2e1ebaf091fc8c0
justinchristopherperry <justin.perry10@outlook.com>
2019-06-21 22:56:49 -0700

    Tutorial (Views)

--- programs/admin.py
+++ programs/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-
-from .models import Program
+from .models import Program, Lift
 
 @admin.register(Program)
 class ProgramAdmin(admin.ModelAdmin):
--- programs/views.py
+++ programs/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
 
-# Create your views here.
+from .models import Program, Lift
+
+def home(request):
+    allPrograms = Program.objects.all()
+    return render(request, 'home.html', {'allPrograms': allPrograms})
+
+def program_detail(request, id):
+    try:
+        program = Program.objects.get(id=id)
+    except Program.DoesNotExist:
+        raise Http404('Program not found.')
+    return render(request, 'program_detail.html', {'program': program})
